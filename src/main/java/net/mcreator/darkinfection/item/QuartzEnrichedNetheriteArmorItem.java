4dff46e9adb89a6bf049105543ba7996d381e0c6
qwsq2008 <quinn@LAPTOP-B9T1VR92>
2022-04-02 22:24:40 -0500

    fixed a problem

--- src/main/java/net/mcreator/darkinfection/item/QuartzEnrichedNetheriteArmorItem.java
+++ src/main/java/net/mcreator/darkinfection/item/QuartzEnrichedNetheriteArmorItem.java
@@ -1,10 +1,20 @@
 
 package net.mcreator.darkinfection.item;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
 
 public abstract class QuartzEnrichedNetheriteArmorItem extends ArmorItem {
-
 	public QuartzEnrichedNetheriteArmorItem(EquipmentSlot slot, Item.Properties properties) {
 		super(new ArmorMaterial() {
 			@Override
@@ -50,10 +60,8 @@ public abstract class QuartzEnrichedNetheriteArmorItem extends ArmorItem {
 	}
 
 	public static class Helmet extends QuartzEnrichedNetheriteArmorItem {
-
 		public Helmet() {
 			super(EquipmentSlot.HEAD, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT));
-
 			setRegistryName("quartz_enriched_netherite_armor_helmet");
 		}
 
@@ -61,14 +69,11 @@ public abstract class QuartzEnrichedNetheriteArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "dark_infection:textures/models/armor/netherite__layer_1.png";
 		}
-
 	}
 
 	public static class Chestplate extends QuartzEnrichedNetheriteArmorItem {
-
 		public Chestplate() {
 			super(EquipmentSlot.CHEST, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT));
-
 			setRegistryName("quartz_enriched_netherite_armor_chestplate");
 		}
 
@@ -76,14 +81,11 @@ public abstract class QuartzEnrichedNetheriteArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "dark_infection:textures/models/armor/netherite__layer_1.png";
 		}
-
 	}
 
 	public static class Leggings extends QuartzEnrichedNetheriteArmorItem {
-
 		public Leggings() {
 			super(EquipmentSlot.LEGS, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT));
-
 			setRegistryName("quartz_enriched_netherite_armor_leggings");
 		}
 
@@ -91,14 +93,11 @@ public abstract class QuartzEnrichedNetheriteArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "dark_infection:textures/models/armor/netherite__layer_2.png";
 		}
-
 	}
 
 	public static class Boots extends QuartzEnrichedNetheriteArmorItem {
-
 		public Boots() {
 			super(EquipmentSlot.FEET, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT));
-
 			setRegistryName("quartz_enriched_netherite_armor_boots");
 		}
 
@@ -106,7 +105,5 @@ public abstract class QuartzEnrichedNetheriteArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "dark_infection:textures/models/armor/netherite__layer_1.png";
 		}
-
 	}
-
 }

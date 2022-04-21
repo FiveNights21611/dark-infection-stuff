6415f2397ff200955d94e5372a0c58adb3943d04
FiveNights21611 <FiveN@DESKTOP-K4GJSLQ>
2022-04-20 21:57:19 -0500

    Adds Darkling Level 1 Advancement

---
+++ src/main/java/net/mcreator/darkinfection/procedures/DarklingLevel1DoItProcedure.java
@@ -0,0 +1,61 @@
+package net.mcreator.darkinfection.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingEvent;
+
+import net.minecraft.world.scores.Scoreboard;
+import net.minecraft.world.scores.Score;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import javax.annotation.Nullable;
+
+import java.util.Iterator;
+
+@Mod.EventBusSubscriber
+public class DarklingLevel1DoItProcedure {
+	@SubscribeEvent
+	public static void onEntityTick(LivingEvent.LivingUpdateEvent event) {
+		Entity entity = event.getEntityLiving();
+		execute(event, entity);
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (new Object() {
+			public int getScore(String score, Entity _ent) {
+				if (_ent instanceof Player _player) {
+					Scoreboard _sc = _player.getScoreboard();
+					Objective _so = _sc.getObjective(score);
+					if (_so != null) {
+						Score _scr = _sc.getOrCreatePlayerScore(_player.getScoreboardName(), _so);
+						return _scr.getScore();
+					}
+				}
+				return 0;
+			}
+		}.getScore("infected", entity) == 1) {
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("dark_infection:darkling_level_1"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					Iterator _iterator = _ap.getRemainingCriteria().iterator();
+					while (_iterator.hasNext())
+						_player.getAdvancements().award(_adv, (String) _iterator.next());
+				}
+			}
+		}
+	}
+}

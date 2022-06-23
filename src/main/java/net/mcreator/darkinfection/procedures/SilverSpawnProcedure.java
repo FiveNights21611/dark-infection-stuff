b8d36254f36fcd8c2d0780ce9fa7ef4051ddbe1a
FiveNights21611 <FiveN@DESKTOP-K4GJSLQ>
2022-06-23 16:14:00 -0500

    Adds BoatGod OP

--- src/main/java/net/mcreator/darkinfection/procedures/SilverSpawnProcedure.java
+++ src/main/java/net/mcreator/darkinfection/procedures/SilverSpawnProcedure.java
@@ -1,6 +1,17 @@
 package net.mcreator.darkinfection.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
 
 import javax.annotation.Nullable;
 

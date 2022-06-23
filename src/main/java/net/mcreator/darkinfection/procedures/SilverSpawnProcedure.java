8702e4a75f97465fd58850316dbc9ec52bcdf303
thnx-cul8ter <lochb@cul8ters-pc>
2022-06-23 16:32:33 -0400

    fixed a  file that didnt compile with the silver stuff (oops)

--- src/main/java/net/mcreator/darkinfection/procedures/SilverSpawnProcedure.java
+++ src/main/java/net/mcreator/darkinfection/procedures/SilverSpawnProcedure.java
@@ -1,6 +1,17 @@
 package net.mcreator.darkinfection.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
 
 import javax.annotation.Nullable;
 
@@ -33,7 +44,7 @@ public class SilverSpawnProcedure {
 			((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY))
 					.enchant(Enchantments.VANISHING_CURSE, 1);
 			((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY))
-					.enchant(Enchantments.UNBREAKING, 10);
+					.enchant(Enchantments.UNBREAKING, 3);
 		}
 	}
 }
